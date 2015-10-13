2329b244f9c530ac4faa2d74f7725574daed1291
Kellen Pierson <kellenpierson@gmail.com>
2015-10-13 14:12:42 -0700

    Added all transit stop pins

--- GetOnThatBus/ViewController.m
+++ GetOnThatBus/ViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "ViewController.h"
+#import <MapKit/MapKit.h>
 
-@interface ViewController ()
+@interface ViewController () <MKMapViewDelegate, CLLocationManagerDelegate>
+
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property (nonatomic) CLLocationManager *locationManager;
+@property (nonatomic) NSDictionary *results;
+@property (nonatomic) NSArray *row;
 
 @end
 
@@ -16,7 +22,60 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    self.locationManager = [CLLocationManager new];
+    self.locationManager.delegate = self;
+    [self.locationManager requestWhenInUseAuthorization];
+    [self.locationManager startUpdatingLocation];
+    self.mapView.showsUserLocation = YES;
+
+    NSURL *url = [NSURL URLWithString:@"https://s3.amazonaws.com/mmios8week/bus.json"];
+
+    NSURLSessionDataTask *task = [[NSURLSession sharedSession] dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+
+        if (data) {
+            self.results = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:nil];
+            self.row = self.results[@"row"];
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [self populatePins];
+            });
+        }
+    }];
+
+    [task resume];
+    
+}
+
+- (void)populatePins {
+
+    for (NSDictionary *busStop in self.row) {
+        MKPointAnnotation *annotation = [MKPointAnnotation new];
+        double lat = [busStop[@"latitude"] doubleValue];
+        double longitude = [busStop[@"longitude"] doubleValue];
+        annotation.coordinate = CLLocationCoordinate2DMake(lat, longitude);
+        annotation.title = busStop[@"cta_stop_name"];
+        [self.mapView addAnnotation:annotation];
+    }
+
 }
 
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
