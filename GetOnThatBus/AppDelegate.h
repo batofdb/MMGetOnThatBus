fc04827c27f947a6f4effbd43c18144d75b772e2
Kellen Pierson <kellenpierson@gmail.com>
2015-10-13 13:39:18 -0700

    Initial Commit

---
+++ GetOnThatBus/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GetOnThatBus
+//
+//  Created by Kellen Pierson on 10/13/15.
+//  Copyright © 2015 Kellen Pierson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GetOnThatBus/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GetOnThatBus
+//
+//  Created by Kellen Pierson on 10/13/15.
+//  Copyright © 2015 Kellen Pierson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
